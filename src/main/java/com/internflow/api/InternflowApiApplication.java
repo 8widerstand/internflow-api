e3e9a83cb4d88783d89b34b242f7b5e02e082596
Willy Fonkou Kamte <kamtowilly18@gmail.com>
2026-08-04 16:00:22 +0200

    feat: add health endpoint

--- src/main/java/com/internflow/api/InternflowApiApplication.java
+++ src/main/java/com/internflow/api/InternflowApiApplication.java
@@ -1,4 +1,4 @@
-package com.internflowapi;
+package com.internflow.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/internflow/api/InternflowApiApplicationTests.java
+++ src/test/java/com/internflow/api/InternflowApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.internflowapi;
+package com.internflow.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
