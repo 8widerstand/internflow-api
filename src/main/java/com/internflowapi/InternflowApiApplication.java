d87342414a4e8c1f38d7fdde8717bf63d53a6fa0
Willy Fonkou Kamte <kamtowilly18@gmail.com>
2026-08-04 15:22:42 +0200

    feat: initialize Spring Boot project with health endpoint

---
+++ src/main/java/com/internflowapi/InternflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.internflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternflowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/internflowapi/InternflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
