0038ef4d3a8408e24136f6615e74d0028466c2ee
Willy Fonkou Kamte <kamtowilly18@gmail.com>
2026-08-04 17:38:37 +0200

    refactor: reuse internship list in service

--- src/main/java/com/internflow/api/internship/InternshipService.java
+++ src/main/java/com/internflow/api/internship/InternshipService.java
@@ -2,22 +2,26 @@ package com.internflow.api.internship;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class InternshipService {
 
+    private final List<InternshipResponse> internships = new ArrayList<>(
+            List.of(new InternshipResponse(1L, "Java Backend Internship", "Docufy", 6),
+                    new InternshipResponse(2L, "Spring Boot Internship", "TechCorp", 3)));
+
+
     public List<InternshipResponse> findAllInternships() {
-        return List.of(new InternshipResponse(1L, "Java Backend Internship", "Docufy", 6),
-                new InternshipResponse(2L, "Spring Boot Internship", "TechCorp", 3));
+        return this.internships;
     }
 
     public InternshipResponse findInternshipById(Long id) {
 
-        if (id.equals(1L)) {
-            return new InternshipResponse(1L, "Java Backend Internship", "Docufy", 6);
-        } else if (id.equals(2L)) {
-            return new InternshipResponse(2L, "Spring Boot Internship", "TechCorp", 3);
-        } else return null;
+        return this.internships.stream()
+                .filter(internship -> internship.id().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
