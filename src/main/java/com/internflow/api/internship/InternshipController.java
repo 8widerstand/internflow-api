76dbbca381ea3decc6e6f0bc68984f99f905dec1
Willy Fonkou Kamte <kamtowilly18@gmail.com>
2026-08-05 16:19:38 +0200

    feat:add JPA persistence for internships

--- src/main/java/com/internflow/api/internship/InternshipController.java
+++ src/main/java/com/internflow/api/internship/InternshipController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,9 +30,12 @@ public class InternshipController {
 
     @PostMapping("/internships")
     public ResponseEntity<InternshipResponse> createInternship(
-            @Valid @RequestBody CreateInternshipRequest createInternshipRequest) {
+            @Valid @RequestBody CreateInternshipRequest createInternshipRequest
+    ) {
         InternshipResponse internship = internshipService.create(createInternshipRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(internship);
+        URI location = URI.create("/internships/" + internship.id());
+
+        return ResponseEntity.created(location).body(internship);
     }
 
     @PutMapping("/internships/{id}")
