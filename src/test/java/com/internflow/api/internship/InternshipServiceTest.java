d0f7f0b87c8fe00f2a623e0410a1eb9be2ca1047
Willy Fonkou Kamte <kamtowilly18@gmail.com>
2026-08-10 13:18:17 +0200

    test: add internship service unit tests

---
+++ src/test/java/com/internflow/api/internship/InternshipServiceTest.java
@@ -0,0 +1,65 @@
+package com.internflow.api.internship;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class InternshipServiceTest {
+    @Mock
+    private InternshipRepository internshipRepository;
+
+    @InjectMocks
+    private InternshipService internshipService;
+
+    @Test
+    void deleteShouldReturnFalseWhenInternshipDoesNotExist() {
+        when(internshipRepository.existsById(1L)).thenReturn(false);
+
+        boolean result = internshipService.delete(1L);
+
+        assertFalse(result);
+        verify(internshipRepository, never()).deleteById(anyLong());
+    }
+
+    @Test
+    void deleteShouldReturnTrueWhenInternshipDoesExist() {
+        when(internshipRepository.existsById(1L)).thenReturn(true);
+
+        boolean result = internshipService.delete(1L);
+
+        assertTrue(result);
+        verify(internshipRepository).deleteById(1L);
+    }
+
+    @Test
+    void findInternshipByIdShouldReturnEmptyWhenInternshipDoesNotExist() {
+        when(internshipRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Optional<InternshipResponse> result = internshipService.findInternshipById(1L);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void findInternshipByIdShouldReturnInternshipWhenInternshipExists() {
+        Internship internship = new Internship("Java Internship", "BMW", 6);
+        when(internshipRepository.findById(1L)).thenReturn(Optional.of(internship));
+
+        Optional<InternshipResponse> result = internshipService.findInternshipById(1L);
+        assertTrue(result.isPresent());
+
+        InternshipResponse response = result.get();
+        assertEquals(internship.getTitle(), response.title());
+        assertEquals(internship.getCompany(), response.company());
+        assertEquals(internship.getDurationInMonths(), response.durationInMonths());
+        assertEquals(internship.getStatus(), response.status());
+    }
+
+}
