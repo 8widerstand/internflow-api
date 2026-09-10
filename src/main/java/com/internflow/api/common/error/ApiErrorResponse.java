7b43feed449c8fd37dbed96f4ca959361d9cfa93
Willy Fonkou Kamte <kamtowilly18@gmail.com>
2026-09-10 18:29:44 +0200

    refactor: rename shared API error response

--- src/main/java/com/internflow/api/common/error/ApiErrorResponse.java
+++ src/main/java/com/internflow/api/common/error/ApiErrorResponse.java
@@ -2,7 +2,7 @@ package com.internflow.api.common.error;
 
 import java.util.Map;
 
-public record ValidationErrorResponse(
+public record ApiErrorResponse(
         String message,
         Map<String, String> errors
 ) {
