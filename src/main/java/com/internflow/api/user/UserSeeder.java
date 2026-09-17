2b867d01abdb5e26d3f1837d5a6ee81c74f9fd48
Willy Fonkou Kamte <kamtowilly18@gmail.com>
2026-09-17 14:19:57 +0200

    feat: store users in the database

---
+++ src/main/java/com/internflow/api/user/UserSeeder.java
@@ -0,0 +1,23 @@
+package com.internflow.api.user;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserSeeder implements CommandLineRunner {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (userRepository.findByUsername("admin").isEmpty()){
+            userRepository.save(new User("admin", passwordEncoder.encode("admin123"), "ADMIN"));
+        }
+    }
+}
